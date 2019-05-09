b8eb845c0eeb82dedf35a23bd32e3aa9061c42c2
felipe Petersen <felipekpetersen@gmail.com>
2019-05-09 13:45:11 -0300

    fluxo login terminado

---
+++ RepublicApp/RepublicApp/Views/Main/CreateAccount/CreateAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateAccountViewController.swift
+//  RepublicApp
+//
+//  Created by Felipe Kaça Petersen on 09/05/19.
+//  Copyright © 2019 Guilherme Enes. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAutoScrollWhenKeyboardShowsUp()
+    }
+    
+    // MARK:- Scroll View Content Inset
+    override func setScrollViewContentInset(_ inset: UIEdgeInsets) {
+        scrollView.contentInset = inset
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RepublicApp/RepublicApp/Views/Main/CreateRepublic/CreateRepublicViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CreateRepublicViewController.swift
+//  RepublicApp
+//
+//  Created by Felipe Kaça Petersen on 09/05/19.
+//  Copyright © 2019 Guilherme Enes. All rights reserved.
+//
+
+import UIKit
+
+class CreateRepublicViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var repNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPassTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAutoScrollWhenKeyboardShowsUp()
+    }
+    
+    // MARK:- Scroll View Content Inset
+    override func setScrollViewContentInset(_ inset: UIEdgeInsets) {
+        scrollView.contentInset = inset
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
