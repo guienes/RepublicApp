354e7d22764890a2ab5dcf517864f85a42c99702
felipe Petersen <felipekpetersen@gmail.com>
2019-05-13 14:48:55 -0300

    tela de produtos

--- RepublicApp/RepublicApp/Views/Home/New Group/ProductsViewController.swift
+++ RepublicApp/RepublicApp/Views/Home/New Group/ProductsViewController.swift
@@ -10,52 +10,114 @@ import UIKit
 
 class ProductsViewController: UIViewController {
     
-    @IBOutlet weak var view1: UIView!
+    @IBOutlet var views: [ProductView]!
+    @IBOutlet var titleViews: [UIView]!
+    @IBOutlet weak var tableViewTeste: UITableView!
+   
+    let productCell = "ProductTableViewCell"
     
-    @IBOutlet weak var constView1: NSLayoutConstraint!
-    @IBOutlet weak var view2: UIView!
-    
-    @IBOutlet weak var constView2: NSLayoutConstraint!
-    
-    @IBOutlet var views: [UIView]!
-    
-    
+    var selectedTag = -1
+    let topConsts = [40, 110, 180, 250]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.viewTaps()
-        // Do any additional setup after loading the view.
+        self.registerViewTag()
+        self.setShadow()
+        self.setupTableView()
+    }
+    
+    func setupTableView() {
+        self.tableViewTeste.delegate = self
+        self.tableViewTeste.dataSource = self
+        tableViewTeste.register(UINib(nibName: productCell, bundle: nil), forCellReuseIdentifier: productCell)
+
+    }
+    
+    func registerViewTag() {
+        var tag = 0
+        for view in titleViews {
+            view.tag = tag
+            tag += 1
+        }
+    }
+    
+    func setShadow() {
+        for view in views {
+            view.setupShadows()
+        }
     }
     
     func viewTaps() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.didTapView(_:)))
         let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.didTapView(_:)))
+        let tap3 = UITapGestureRecognizer(target: self, action: #selector(self.didTapView(_:)))
+        let tap4 = UITapGestureRecognizer(target: self, action: #selector(self.didTapView(_:)))
 
-        self.views[0].addGestureRecognizer(tap)
-        self.views[1].addGestureRecognizer(tap2)
+        self.titleViews[0].addGestureRecognizer(tap)
+        self.titleViews[1].addGestureRecognizer(tap2)
+        self.titleViews[2].addGestureRecognizer(tap3)
+        self.titleViews[3].addGestureRecognizer(tap4)
     }
     
     @objc func didTapView(_ sender: UITapGestureRecognizer) {
 //        if let constraint = sender.view?.constraints.filter{ $0.identifier == "checkmarkLeftMargin" }.first { }
 //        let checkmarkImageViewLeftMargin = sender.view?.constraints.first { $0.identifier == "checkmarkLeftMargin" }
-        self.sendAllDown()
-        sender.view?.getAllConstraints().first { $0.identifier == "top" }?.constant = 0
-        sender.view?.getAllConstraints().first { $0.identifier == "bottom" }?.constant = 32
-        
+        if selectedTag != -1 {
+            sendAllUp()
+            selectedTag = -1
+        } else {
+            self.sendAllDown()
+            self.views?[sender.view?.tag ?? 0].getAllConstraints().first { $0.identifier == "top" }?.constant = 0
+            self.views?[sender.view?.tag ?? 0].getAllConstraints().first { $0.identifier == "bottom" }?.constant = 100
+            self.selectedTag = sender.view?.tag ?? -1
+        }
 //        self.constView2.constant = sender.view?.frame.maxY ?? 0
 //        self.constView2.constant = -100
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 0.6) {
             self.view.layoutIfNeeded()
+        
         }
     }
     
     func sendAllDown() {
+        var const:CGFloat = 130
         for view in views {
-            view.getAllConstraints().first { $0.identifier == "top" }?.constant = self.view.frame.height - 160
+            view.getAllConstraints().first { $0.identifier == "top" }?.constant = self.view.frame.height - const
             view.getAllConstraints().first { $0.identifier == "bottom" }?.constant = -50
+            const -= 10
+        }
+    }
+    
+    func sendAllUp() {
+        for i in views.indices {
+            views[i].getAllConstraints().first { $0.identifier == "top" }?.constant = CGFloat(self.topConsts[i])
+            views[i].getAllConstraints().first { $0.identifier == "bottom" }?.constant = -40
         }
     }
 
+}
+
+extension ProductsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewTeste.dequeueReusableCell(withIdentifier: productCell, for: indexPath) as! ProductTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("comi o cu do enes")
+    }
+    
+    
 }
 
 extension UIView {
