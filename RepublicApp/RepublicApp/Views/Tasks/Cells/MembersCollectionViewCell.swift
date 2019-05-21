027aaef4ef9cf1864d38d7e70cc34548e628116c
Amaury A V A Souza <amauryavasouza@gmail.com>
2019-05-21 14:12:27 -0300

    Celula de membros feita

---
+++ RepublicApp/RepublicApp/Views/Tasks/Cells/MembersCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MembersCollectionViewCell.swift
+//  RepublicApp
+//
+//  Created by Amaury A V A Souza on 20/05/19.
+//  Copyright © 2019 Guilherme Enes. All rights reserved.
+//
+
+import UIKit
+
+class MembersCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var Foto: ProductView!
+    @IBOutlet weak var Celula: UIView!
+    @IBOutlet weak var Transparencia: ProductView!
+    
+    @IBOutlet weak var Nome: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    
+    }
+
+
+}
